71d9c6dd81c67be36bc79fa0c3e5e0cc56cc65e5
wooseok <gowoo97@gmail.com>
2023-05-22 22:14:52 +0900

    헤더 디자인

---
+++ react/shopping_react/src/components/header/Header.js
@@ -0,0 +1,27 @@
+import styles  from './Header.module.css';
+import { Link } from "react-router-dom";
+
+
+const Header=()=>{
+    return(
+        <>
+        <header>
+           <div className={ styles.headerTop }>
+                
+                <Link><span className={styles.headerTopA}>Login</span></Link>
+                <Link><span className={styles.headerTopA}>MyPage</span></Link>
+                <Link><span className={styles.headerTopA}>Cart</span></Link>
+
+            </div>
+            <div className={styles.headerTitle}>
+                
+                <h1>SHOP</h1>
+                
+                
+            </div>
+        </header>
+   </>
+    );
+}
+
+export default Header;
