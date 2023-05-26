ccace4ff94a5b25ed09666802eb139ea87935996
wooseok <gowoo97@gmail.com>
2023-05-26 17:49:12 +0900

    슬라이드가 되긴됨

--- react/shopping_react/src/components/content/Slide.js
+++ react/shopping_react/src/components/content/Slide.js
@@ -1,7 +1,46 @@
+import { useEffect } from 'react';
 import styles from './Slide.module.css';
 
 const Slide = () =>{
     
+        
+
+    useEffect(
+        ()=>{
+            const items=document.getElementsByClassName( styles.slide_item );
+            let cnt=0;
+           const interval= setInterval(()=>{
+                console.log(items);
+                
+
+                for(let i=0;i<items.length;i++){
+                    let now=items[i].style.left;
+                    console.log(now);
+                    console.log(cnt);
+                    if(now === ""){
+                        now=0;
+                    }
+                    else if(cnt == 4){
+                        cnt=0;
+                        now=976;
+                    }
+                    else{
+                        now=parseInt(now);
+                    }
+                    now=now-976;
+                   
+                    items[i].setAttribute("style","left:"+now+"px");
+                }
+                cnt++;
+            },5000);
+            return  () =>{
+                clearInterval(interval);
+             } ;
+        },[]
+    );
+   
+    
+
     return (
         <div className={ styles.slide }> 
             <div className={ styles.slide_container}>
