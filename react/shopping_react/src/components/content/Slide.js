d3d0388d8822884decf11839ce7295e9d3bc022d
wooseok <gowoo97@gmail.com>
2023-05-28 14:26:47 +0900

    슬라이드

--- react/shopping_react/src/components/content/Slide.js
+++ react/shopping_react/src/components/content/Slide.js
@@ -1,42 +1,61 @@
 import { useEffect } from 'react';
 import styles from './Slide.module.css';
+import {useState} from 'react';
 
 const Slide = () =>{
-    
+    const [count,setCount] =useState(0);
+
+    const rmDot= ()=>{
+        const dots=document.getElementsByClassName(styles.dot);
+        for(let i =0;i<dots.length;i++){
+            let val=dots[i].style.backgroundColor;
+            if(val === 'black'){
+                dots[i].setAttribute("style","background-color:white");
+            }
+        }
+    }
+
+    const next= ()=>{
+        const items=document.getElementsByClassName( styles.slide_item );
+        const dots=document.getElementsByClassName( styles.dot);
         
+        setCount(count+1);
+        rmDot();
+        dots[(count+1)%5].setAttribute("style","background-color:black");
+        console.log(count);
+        if(count === 4){
+            for(let i =0 ;i<items.length;i++){
+                items[i].setAttribute("style","left:0px");
+            }
+            setCount(0);
+        }
+        else{
+            for(let i=0;i<items.length;i++){
+                let now=items[i].style.left;//현재 위치
+                if(now === ""){//현재 움직이지 않았다면
+                    now=-976;
+                }
+                else{
+                    now=parseInt(now)-976;
+                }
+    
+                items[i].setAttribute("style","left:"+now+"px");
+            }
+        }
+
+
+        
+    }    
+
 
     useEffect(
         ()=>{
-            const items=document.getElementsByClassName( styles.slide_item );
-            let cnt=0;
-           const interval= setInterval(()=>{
-                console.log(items);
-                
-
-                for(let i=0;i<items.length;i++){
-                    let now=items[i].style.left;
-                    console.log(now);
-                    console.log(cnt);
-                    if(now === ""){
-                        now=0;
-                    }
-                    else if(cnt == 4){
-                        cnt=0;
-                        now=976;
-                    }
-                    else{
-                        now=parseInt(now);
-                    }
-                    now=now-976;
-                   
-                    items[i].setAttribute("style","left:"+now+"px");
-                }
-                cnt++;
-            },5000);
+            
+           const interval= setInterval(next,5000);
             return  () =>{
                 clearInterval(interval);
              } ;
-        },[]
+        },[count]
     );
    
     
@@ -51,11 +70,11 @@ const Slide = () =>{
                 <div className={styles.slide_item}>5</div>
             </div>
             <div className={styles.dots}>
+                <span className={styles.dot} style={{backgroundColor:"black"}}></span>
                 <span className={styles.dot}></span>
                 <span className={styles.dot}></span>
                 <span className={styles.dot}></span>
                 <span className={styles.dot}></span>
-
             </div>
         </div>
     );
